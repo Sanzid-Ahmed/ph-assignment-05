d4739b677f6b959bcbc91dfa6f43404cfe2a3137
Sanzid Ahmed <sanzidahmed313@gmail.com>
2025-08-27 17:34:53 +0600

    added history section

--- script/call.js
+++ script/call.js
@@ -15,8 +15,30 @@ const coins = function(name, number){
     }
     else{
         alert(`❌ You do not have enough coin. You need 20 coins for call`);
-    }       
+    }
+    
+    history(name, number);
 };
+
+const history = function(name, number){
+    const now = new Date();
+    const time = now.toLocaleTimeString(); 
+
+
+    const historyContainer = document.getElementById("history-container");
+
+    const historyDiv = document.createElement("Div");
+    historyDiv.className = " bg-gray-200 rounded-[8px] flex justify-between items-center gap-[16px] p-[16px] mb-[8px]";
+    historyDiv.innerHTML = `
+        <div>
+            <h1 class="font-bold">${name}</h1>
+            <p>${number}</p>
+        </div>
+        <p>${time}</p>
+    `;
+
+    historyContainer.appendChild(historyDiv);
+}
 
 document.querySelectorAll(".call-btn")
 .forEach((button) => {
